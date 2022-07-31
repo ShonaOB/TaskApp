e19080c90b8a741aa5d99016c89d4babdee4da5d
ShonaOB <shona-obriendev@outlook.com>
2022-07-31 15:50:08 +0000

    Amend spelling error in __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -13,7 +13,7 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startstwith("postgres://"):
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgred://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri #heroku
 
